2527b7bf59ea7b5d1de9df57922fdc3a8ea87009
darya.zhelyaeva <darya.zhelyaeva@south.rt.ru>
2024-10-13 11:01:02 +0300

    Third homework by topic 'spring-boot' - preparation

---
+++ hw03/src/main/java/ru/otus/hw/Application.java
@@ -0,0 +1,18 @@
+package ru.otus.hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import ru.otus.hw.service.TestRunnerService;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+
+        //Создать контекст Spring Boot приложения
+        ApplicationContext context = null;
+        var testRunnerService = context.getBean(TestRunnerService.class);
+        testRunnerService.run();
+
+    }
+}
