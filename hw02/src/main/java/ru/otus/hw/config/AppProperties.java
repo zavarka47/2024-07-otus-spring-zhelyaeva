f8bf48fcbd63c9f7482883c227a9c4c3739c620b
darya.zhelyaeva <darya.zhelyaeva@south.rt.ru>
2024-10-13 10:10:09 +0300

    Done second homework by topic 'annotation-config'

---
+++ hw02/src/main/java/ru/otus/hw/config/AppProperties.java
@@ -0,0 +1,18 @@
+package ru.otus.hw.config;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Data
+@PropertySource(value = "classpath:application.properties", ignoreResourceNotFound = true)
+@Configuration
+public class AppProperties implements TestConfig, TestFileNameProvider {
+
+    @Value("${test.rightAnswersCountToPass}")
+    private int rightAnswersCountToPass;
+
+    @Value("${test.fileName}")
+    private String testFileName;
+}
---
+++ hw02/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -0,0 +1,49 @@
+package ru.otus.hw.dao;
+
+import com.opencsv.bean.CsvToBeanBuilder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
+import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.Objects;
+
+@RequiredArgsConstructor
+@Component
+public class CsvQuestionDao implements QuestionDao {
+    private final TestFileNameProvider fileNameProvider;
+
+    @Override
+    public List<Question> findAll() {
+        // Использовать CsvToBean
+        // https://opencsv.sourceforge.net/#collection_based_bean_fields_one_to_many_mappings
+        // Использовать QuestionReadException
+        // Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
+        List<Question> result = null;
+        try (var questionStream = Objects.requireNonNull(getClass().getClassLoader()
+                .getResourceAsStream(fileNameProvider.getTestFileName()))) {
+            result = new CsvToBeanBuilder<QuestionDto>(new BufferedReader(new InputStreamReader(questionStream)))
+                    .withType(QuestionDto.class)
+                    .withSeparator(';')
+                    .withSkipLines(1)
+                    .withOrderedResults(false)
+                    .build()
+                    .parse()
+                    .stream()
+                    .map(QuestionDto::toDomainObject)
+                    .toList();
+
+        } catch (NullPointerException | IOException e) {
+            throw new QuestionReadException(e.getMessage());
+        }
+
+        return result;
+
+    }
+}
