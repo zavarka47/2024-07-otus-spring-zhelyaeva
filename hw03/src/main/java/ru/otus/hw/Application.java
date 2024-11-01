d38975149797ef9599dcdfd01882dc71ebe22a6b
Darya Zhelyaeva <darya.zhelyaeva@south.rt.ru>
2024-11-01 11:56:56 +0300

    Update after remark third homework by topic 'spring-boot'

--- hw03/src/main/java/ru/otus/hw/Application.java
+++ hw03/src/main/java/ru/otus/hw/Application.java
@@ -3,9 +3,7 @@ package ru.otus.hw;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.ApplicationContext;
 import ru.otus.hw.config.AppProperties;
-import ru.otus.hw.service.TestRunnerService;
 
 @SpringBootApplication
 @EnableConfigurationProperties(AppProperties.class)
@@ -13,7 +11,7 @@ public class Application {
     public static void main(String[] args) {
 
         //Создать контекст Spring Boot приложения
-        ApplicationContext context = SpringApplication.run(Application.class);
+        SpringApplication.run(Application.class);
 
     }
 }
